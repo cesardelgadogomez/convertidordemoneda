dd56a955a60f611158ca3d36ad0838a410945946
cesardelgadogomez <cesadelgado2110@gmail.com>
2025-04-14 13:16:56 -0500

    implementado el dark mode

--- js/toogle-theme.js
+++ js/toogle-theme.js
@@ -1,25 +1,17 @@
-const toogleTheme = document.getElementById("toogle-theme");
-const body = document.body;
+document.addEventListener('DOMContentLoaded', () => {
+  const themeToggle = document.getElementById('theme-toggle');
+  const body = document.body;
 
-let darkMode = localStorage.getItem("dark-mode");
-
-function activarDarkMode () {
-  body.classList.add("dark-mode");
-  localStorage.setItem("dark-mode", "activado");  
-}
-
-function desactivarDarkMode () {
-  body.classList.remove("dark-mode");
-  localStorage.setItem("dark-mode", "desactivado");  
-}
-
-toogleTheme.addEventListener("click", () => {
-
-  darkMode = localStorage.getItem("dark-mode");
-
-  if (darkMode !== "activado") {
-    activarDarkMode();
-  } else {
-    desactivarDarkMode();
+  if (localStorage.getItem('dark-mode') === 'activado') {
+    body.classList.add('dark-mode');
   }
+
+  themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+      localStorage.setItem('dark-mode', 'activado');
+    } else {
+      localStorage.setItem('dark-mode', 'desactivado');
+    }
+  });
 });
